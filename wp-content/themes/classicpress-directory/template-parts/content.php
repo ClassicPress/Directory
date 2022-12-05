eeadff29eada1f1fb1e8abe6e77edfc5e84de804
Viktor Nagornyy <viktor@projectarmy.net>
2022-12-05 11:29:43 -0500

    Updated single software page layout
    - Updated layout to include sidebar with software meta info higher on the page, so user doesn't need to scroll down to download it.
    - Minor issues fixed
    - Added white download icon

--- wp-content/themes/classicpress-directory/template-parts/content.php
+++ wp-content/themes/classicpress-directory/template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts
  *
@@ -9,135 +10,148 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('single-software'); ?>>
 
 	<?php
-	if ( is_singular() ) :
+	if (is_singular()) :
 	?>
 
 		<header>
-			<?php the_title( '<h1>', '</h1>' ); ?>
+			<div class="header-content">
+				<?php the_title('<h1>', '</h1>'); ?>
+				<div class="software-author-link"><?php _e('Developed by', 'classicpress'); ?> <span><?php echo get_the_author_posts_link(); ?></span></div>
+			</div>
+			<div class="software-action">
+				<a href="<?php echo get_post_meta($post->ID, 'download_link', true); ?>" title="<?php _e('Download', 'classicpress'); ?> <?php echo esc_attr($post->post_title); ?>" class="btn" role="button"><img src="<?php echo esc_url(get_template_directory_uri() . '/images/download-white.svg'); ?>" alt="download" width="18" height="18" aria-hidden="true"> <?php _e('Download', 'classicpress'); ?></a>
+			</div>
 		</header>
 
-		<div id="software" class="software">
+		<section id="software" class="software">
 			<h2 class="h5 description">
-			<?php
-			switch (get_post_type()) {
-				case 'plugin':
-					esc_html_e( 'Plugin Description', 'classicpress' );
-					break;
-				case 'theme':
-					esc_html_e( 'Theme Description', 'classicpress' );
-					break;
-				case 'snippet':
-					esc_html_e( 'Snippet Description', 'classicpress' );
-					break;
-			}
-			?>
+				<?php
+				switch (get_post_type()) {
+					case 'plugin':
+						esc_html_e('Plugin Description', 'classicpress');
+						break;
+					case 'theme':
+						esc_html_e('Theme Description', 'classicpress');
+						break;
+					case 'snippet':
+						esc_html_e('Snippet Description', 'classicpress');
+						break;
+				}
+				?>
 			</h2>
-			<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'classicpress' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
 
-			if ( $post->post_type === 'plugin' ) {
-			?>
+			<div class="readme-md-content">
+				<?php
+				the_content(sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'classicpress'),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				));
+				?>
+			</div>
 
-				<h2 class="h5"><?php _e( 'Categories', 'classicpress' ); ?></h2>
-				<?php the_category(); ?>
+		</section>
 
-			<?php
-			}
-			elseif ( $post->post_type === 'snippet' ) {
-			?>
+		<aside>
+			<h2 class="screen-reader-text">Meta</h2>
+			<ul class="aside-items">
+				<li class="aside-item">
+					<?php _e('Version', 'classicpress'); ?>
+					<span class="item-data"><?php echo esc_html(get_post_meta($post->ID, 'current_version', true)); ?></span>
+				<li>
+				<li class="aside-item">
+					<?php _e('ClassicPress Version', 'classicpress'); ?>
+					<span class="item-data"><?php echo esc_html(get_post_meta($post->ID, 'requires_cp', true)) . '.0'; ?></span>
+				</li>
+				<?php
+				if ($post->post_type === 'plugin') {
+				?>
+					<li class="aside-item">
+						<?php _e('Categories', 'classicpress'); ?>
+						<span class="item-data"><?php the_category(); ?></span>
+					</li>
 
-				<h2 class="h5"><?php _e( 'Tags', 'classicpress' ); ?></h2>
-				<?php the_tags( '' ); ?>
+				<?php
+				} elseif ($post->post_type === 'snippet') {
+				?>
+					<li class="aside-item">
+						<?php _e('Tags', 'classicpress'); ?>
+						<span class="item-data"><?php the_tags('<div class="tags"><span class="tag">', '</span><span class="tag">', '</span></div>'); ?></span>
+					</li>
 
-			<?php
-			}
-			?>
+				<?php
+				}
+				?>
+			</ul>
+		</aside>
 
-			<h2 class="h5"><?php _e( 'Developer', 'classicpress' ); ?></h2>
-			<?php the_author_posts_link(); ?>
-
-			<h2 class="h5"><?php _e( 'Current Version', 'classicpress' ); ?></h2>
-			<?php echo esc_html( get_post_meta( $post->ID, 'current_version', true ) ); ?>
-
-			<h2 class="h5"><?php _e( 'Minimum ClassicPress Version', 'classicpress' ); ?></h2>
-			<?php echo esc_html( get_post_meta( $post->ID, 'requires_cp', true ) ) . '.0'; ?>
-
-			<h2 class="h5"><?php _e( 'Download Link', 'classicpress' ); ?></h2>
-			<?php echo make_clickable( esc_url( get_post_meta( $post->ID, 'download_link', true ) ) ); ?>
-
-		</div>
-	
 	<?php
 	else :
 	?>
-		
+
 		<header>
-			<?php the_title( '<h2  class="h3"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );	?>
+			<?php the_title('<h2  class="h3"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');	?>
 			<span class="software-author-link"><?php echo get_the_author_posts_link(); ?></span>
 		</header>
 
-		<p><?php kts_excerpt_fallback( $post ); ?></p>
-		
+		<p><?php kts_excerpt_fallback($post); ?></p>
+
 		<div class="software-item-meta">
 			<div class="meta-item left">
-				<a href="<?php echo esc_url( get_post_meta( $post->ID, 'download_link', true ) ); ?>" title="<?php _e( 'Download', 'classicpress' ) . ' ' . esc_attr( $post->post_title ); ?>" target="_blank" rel="noopener noreferrer">
-					<img src="<?php echo esc_url( get_template_directory_uri() . '/images/download.svg' ); ?>" alt="download" width="18" height="18">
-					<span><?php _e( 'Download', 'classicpress' ); ?></span>
+				<a href="<?php echo esc_url(get_post_meta($post->ID, 'download_link', true)); ?>" title="<?php _e('Download', 'classicpress') . ' ' . esc_attr($post->post_title); ?>" target="_blank" rel="noopener noreferrer">
+					<img src="<?php echo esc_url(get_template_directory_uri() . '/images/download.svg'); ?>" alt="<?php _e('Download', 'classicpress'); ?>" width="18" height="18" aria-hidden="true">
+					<span><?php _e('Download', 'classicpress'); ?></span>
 				</a>
 			</div>
 			<div class="meta-item right">
-				<a href="#info-<?php echo absint( $post->ID ); ?>" title="<?php _e( 'More information about', 'classicpress' ); ?> <?php echo esc_attr( $post->post_title ); ?>" class="info-button" data-info="info-<?php echo absint( $post->ID ); ?>">
-					<img src="<?php echo esc_url( get_template_directory_uri() . '/images/info.svg' ); ?>" alt="download" width="18" height="18">
-					<span><?php _e( 'More Info', 'classicpress' ); ?></span>
+				<a href="#info-<?php echo absint($post->ID); ?>" title="<?php _e('More information about', 'classicpress'); ?> <?php echo esc_attr($post->post_title); ?>" class="info-button" data-info="info-<?php echo absint($post->ID); ?>">
+					<img src="<?php echo esc_url(get_template_directory_uri() . '/images/info.svg'); ?>" alt="<?php _e('Info', 'classicpress'); ?>" width="18" height="18" aria-hidden="true">
+					<span><?php _e('More Info', 'classicpress'); ?></span>
 				</a>
 			</div>
 		</div>
-		
+
 	<?php
 	endif;
 
-	if ( ! is_singular() ) {
+	if (!is_singular()) {
 	?>
 
-	<footer>
+		<footer>
 
-		<?php // Add update button if author archive is of the current user
-		kts_render_software_update_link_form( $post );
-		?>
-
-		<div id="info-<?php echo absint( $post->ID ); ?>" class="info-panel" hidden>
-
-			<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'classicpress' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-			get_the_title()
-			) );
+			<?php // Add update button if author archive is of the current user
+			kts_render_software_update_link_form($post);
 			?>
 
-		</div>
-	</footer>
+			<div id="info-<?php echo absint($post->ID); ?>" class="info-panel" hidden>
+
+				<?php
+				the_content(sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__('Continue reading<span class="screen-reader-text"> "%s"</span>', 'classicpress'),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				));
+				?>
+
+			</div>
+		</footer>
 
 	<?php
 	}
