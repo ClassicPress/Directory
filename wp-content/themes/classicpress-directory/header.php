d6053f180cfebcce6f6c2f2adf2333e7bebd939a
Viktor Nagornyy <viktor@projectarmy.net>
2022-12-06 15:18:35 -0500

    JS fixes

--- wp-content/themes/classicpress-directory/header.php
+++ wp-content/themes/classicpress-directory/header.php
@@ -87,12 +87,14 @@
 		<script>
 			(function($) {
 				$( ".open-search a" ).click(function(e) {
+					console.log('open');
 					e.preventDefault();
 					$( "#masthead-inner" ).slideUp();
 					$( "#search-form-wrapper" ).slideDown();
 					$( "#masthead" ).css('background', '#fff');
 				});
 				$( ".close-search button" ).click(function(e) {
+					console.log('close');
 					$( "#search-form-wrapper" ).slideUp();
 					$( "#masthead-inner" ).slideDown();
 					$( "#masthead" ).removeAttr('style');
