56913600b5dfb18a7bd4a6b463bff6e4c12adcaa
Viktor Nagornyy <vnagornyy@viktorix.com>
2022-12-06 09:32:35 -0500

    Updated single software page layout (#35)
    * Updated single software page layout
    
    - Updated layout to include sidebar with software meta info higher on the page, so user doesn't need to scroll down to download it.
    - Minor issues fixed
    - Added white download icon
    
    * Fixing global CSS styles
    
    Fixed background color and padding issues.
    
    * Conditional class
    
    Added conditional class for single pages.
    
    * Conditional byline
    
    Added conditional byline and CSS fixes for tags.
    
    * Tag font weight fix
    
    Fixing font weight.
    
    * Description title margin
    
    Fixing margin for description title.
    
    * Added repository link
    
    Added a link to software's repository to sidebar.
    
    * Repo url fixes
    
    Fixed URL not displayed and CSS fixes.
    
    * CSS fix
    
    Fixed font weight.
    
    * Fix for tag class
    
    Can't use .tag class as it affects tag archive page elements. Fixed.
    
    * Fix typo
    
    Fixed a typo.
    
    * Background color fix
    
    Removed non-white bg color.
    
    * Page template 
    
    Introduced narrow width page template to allow non-software pages to have a narrow width as needed.
    
    * Fix fieldset color
    
    Fixed fieldset border color.
    
    * Mobile fixes
    
    Fixing mobile issues.
    
    * More mobile fixes
    
    Additional mobile fixes.
    
    * Mobile fixes
    
    Fixed grid-areas.
    
    * Aside item fix
    
    Fixed margin.

---
+++ wp-content/themes/classicpress-directory/page-narrow.php
@@ -0,0 +1,28 @@
+<?php
+/* Template Name: Narrow Content */
+
+get_header();
+?>
+
+	<div id="primary" class="narrow-page">
+		<main id="main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
