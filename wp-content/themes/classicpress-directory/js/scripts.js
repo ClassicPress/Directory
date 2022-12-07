d6278dc0fa40b66cb45d9cb3ff93c67e64f4bcc0
Viktor Nagornyy <vnagornyy@viktorix.com>
2022-12-07 00:29:19 -0500

    Search experience enhancements (#40)
    * Reduced faceted search fields
    * Disabled/removed Search & Filter plugin (it doesn't affect API)
    * Set up Relevanssi plugin to index API slug and provide better results
    * Moved search to primary menu

--- wp-content/themes/classicpress-directory/js/scripts.js
+++ wp-content/themes/classicpress-directory/js/scripts.js
@@ -63,4 +63,23 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	}, false);
 
+	/* NAV SEARCH */
+	document.querySelector('.open-search a').addEventListener('click', function(e) {
+		e.preventDefault();
+		document.getElementById('masthead-inner').style.display = 'none';
+		document.getElementById('search-form-wrapper').removeAttribute('style');
+		document.getElementById('masthead').classList.add('masthead-search');
+		document.getElementById('ofsearch').focus();
+	}, false);
+
+	const close = document.querySelector('.close-search button');
+	if( close ){
+		close.addEventListener('click', function() {
+			document.getElementById('masthead-inner').removeAttribute('style');
+			document.getElementById('search-form-wrapper').style.display = 'none';
+			document.getElementById('masthead').classList.remove('masthead-search');
+			document.querySelector('.open-search a').focus();
+		}, false);
+	}
+
 });
