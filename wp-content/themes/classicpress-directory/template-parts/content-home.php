4739b8f58b56bc062a9e430a3ab0a33d2a780e28
Viktor Nagornyy <viktor@projectarmy.net>
2023-05-29 13:59:17 -0400

    fix: Added links to featured section headings

--- wp-content/themes/classicpress-directory/template-parts/content-home.php
+++ wp-content/themes/classicpress-directory/template-parts/content-home.php
@@ -12,13 +12,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
     <section class="home-search">
-        <h1>ClassicPress Directory</h1>
-        <p class="home-lead">Extend your ClassicPress website with <mark><?php echo do_shortcode('[total_plugin_count]'); ?> plugins</mark> and <mark><?php echo do_shortcode('[total_theme_count]'); ?> themes</mark>.</p>
+        <h1><?php _e('ClassicPress Directory', 'classicpress'); ?></h1>
+        <p class="home-lead"><p><?php printf(__('Extend your ClassicPress website with <mark>%s plugins</mark> and <mark>%s themes</mark>.', 'classicpress'), do_shortcode('[total_plugin_count]'), do_shortcode('[total_theme_count]')); ?></p>
         <div class="home-search-form">
             <form action="/" method="get">
                 <div role="search">
                     <div class="home-search-input">
-                        <label for="ofsearch" class="screen-reader-text">Search plugins and themes</label>
+                        <label for="ofsearch" class="screen-reader-text"><?php _e('Search plugins and themes', 'classicpress'); ?></label>
                         <input type="text" id="ofsearch" name="s" placeholder="Search plugins and themes" value="" required="required">
                         <input type="hidden" name="post_types" value="plugin,theme">
                     </div>
@@ -31,14 +31,22 @@
     </section>
     <hr>
     <section class="featured-software">
-        <?php
-        echo "<h2>Featured Plugins</h2>";
-        get_random_plugin_posts();
-        echo "<hr>";
-        echo "<h2>Featured Themes</h2>";
-        get_random_theme_posts();
-        ?>
+        <header class="featured-heading">
+            <h2><?php _e('Featured Plugins', 'classicpress'); ?></h2>
+            <a href="/plugins"><?php _e('Explore all plugins', 'classicpress'); ?></a>
+        </header>
+        <?php get_random_plugin_posts(); ?>
     </section>
+    <hr>
+
+    <section class="featured-software">
+        <header class="featured-heading">
+            <h2><?php _e('Featured Themes', 'classicpress'); ?></h2>
+            <a href="/themes"><?php _e('Explore all themes', 'classicpress'); ?></a>
+        </header>
+        <?php get_random_theme_posts(); ?>
+    </section>
+
     <section class="home-content">
         <?php the_content(); ?>
     </section>
