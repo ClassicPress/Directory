ccc19d96426a8c6e3c8ce46243bd007f6e092b5d
Viktor Nagornyy <viktor@projectarmy.net>
2023-06-02 11:44:06 -0400

    fix: commercial notice text change and escaping

--- wp-content/themes/classicpress-directory/template-parts/content.php
+++ wp-content/themes/classicpress-directory/template-parts/content.php
@@ -31,7 +31,7 @@
 				</div>
 			</div>
 			<div class="software-action">
-				<a href="<?php echo esc_url(get_post_meta($post->ID, 'download_link', true)); ?>" title="<?php _e('Download', 'classicpress'); ?> <?php echo esc_attr($post->post_title); ?>" class="btn" role="button"><img src="<?php echo esc_url(get_template_directory_uri() . '/images/download-white.svg'); ?>" alt="download" width="18" height="18" aria-hidden="true"> <?php _e('Download', 'classicpress'); ?></a>
+				<a href="<?php echo esc_url(get_post_meta($post->ID, 'download_link', true)); ?>" title="<?php esc_attr_e('Download', 'classicpress'); ?> <?php echo esc_attr($post->post_title); ?>" class="btn" role="button"><img src="<?php echo esc_url(get_template_directory_uri() . '/images/download-white.svg'); ?>" alt="<?php esc_attr_e('Download', 'classicpress'); ?>" width="18" height="18" aria-hidden="true"> <?php _e('Download', 'classicpress'); ?></a>
 			</div>
 		</header>
 
@@ -82,16 +82,16 @@
 					if (isset($published_at)) : ?>
 				<li class="aside-item">
 					<?php _e('Last Updated', 'classicpress'); ?>
-					<span class="item-data"><time datetime="<?php esc_attr_e($published_at_atom); ?>" title="<?php esc_attr_e($published_at_human); ?>"><?php esc_html_e($published_at_diff); ?></time> <?php _e('ago', 'classicpress'); ?></span>
+					<span class="item-data"><time datetime="<?php echo esc_attr($published_at_atom); ?>" title="<?php echo esc_attr($published_at_human); ?>"><?php echo esc_html($published_at_diff); ?></time> <?php _e('ago', 'classicpress'); ?></span>
 				</li>
 			<?php endif; ?>
 			<li class="aside-item">
 				<?php _e('ClassicPress Version', 'classicpress'); ?>
-				<span class="item-data"><?php echo esc_html(get_post_meta($post->ID, 'requires_cp', true)) . '.0'; ?></span>
+				<span class="item-data"><?php echo esc_html(get_post_meta($post->ID, 'requires_cp', true) . '.0'); ?></span>
 			</li>
 			<li class="aside-item">
 				<?php _e('PHP Version', 'classicpress'); ?>
-				<span class="item-data"><?php echo esc_html(get_post_meta($post->ID, 'requires_php', true)) . '.0'; ?></span>
+				<span class="item-data"><?php echo esc_html(get_post_meta($post->ID, 'requires_php', true) . '.0'); ?></span>
 			</li>
 			<?php
 			if ($post->post_type === 'plugin') {
@@ -130,13 +130,13 @@
 			?>
 			<li class="aside-item">
 				<?php _e('Repository', 'classicpress'); ?>
-				<span class="item-data repo-link"><a href="<?php echo esc_url($repo['url']); ?>" target="_blank" rel="noopener noreferrer" title="<?php _e('Visit repository', 'classicpress'); ?>"><img src="<?php echo esc_url(get_template_directory_uri() . '/images/' . $repo['name'] . '.svg'); ?>" alt="<?php echo $repo['name']; ?>" width="14" height="14" aria-hidden="true"><?php echo $repo['name']; ?></a></span>
+				<span class="item-data repo-link"><a href="<?php echo esc_url($repo['url']); ?>" target="_blank" rel="noopener noreferrer" title="<?php esc_attr_e('Visit repository', 'classicpress'); ?>"><img src="<?php echo esc_url(get_template_directory_uri() . '/images/' . $repo['name'] . '.svg'); ?>" alt="<?php echo esc_attr($repo['name']); ?>" width="14" height="14" aria-hidden="true"><?php echo esc_html($repo['name']); ?></a></span>
 			</li>
 			<?php $active_installations = filter_var(get_post_meta($post->ID, 'active_installations', true), FILTER_VALIDATE_INT);
 			if ($active_installations) : ?>
 				<li class="aside-item">
 					<?php _e('Active Installations', 'classicpress'); ?>
-					<span class="item-data repo-link" title="<?php echo $active_installations; ?>"><?php echo human_readable_number($active_installations); ?></span>
+					<span class="item-data repo-link" title="<?php esc_attr_e($active_installations); ?>"><?php echo esc_html(human_readable_number($active_installations)); ?></span>
 				</li>
 			<?php endif; ?>
 			</ul>
@@ -147,8 +147,8 @@
 			?>
 				<div class="premium-notice">
 					<h3><?php printf(__('Commercial %s', 'classicpress'), get_post_type()); ?></h3>
-					<p><?php printf(__('This %s is free, but it offers a paid version, addons, or requires a paid account.', 'classicpress'), get_post_type()); ?>
-						<a href="<?php echo esc_url($premium_link); ?>" target="blank" class="external" title="<?php _e('Learn more', 'classicpress'); ?>" rel="nofollow ugc external noopener"><?php _e('Learn more', 'classicpress'); ?></a>
+					<p><?php printf(__('This %s is free, but it offers a paid version, addons, support, or requires a paid account.', 'classicpress'), get_post_type()); ?>
+						<a href="<?php echo esc_url($premium_link); ?>" target="blank" class="external" title="<?php esc_attr_e('Learn more', 'classicpress'); ?>" rel="nofollow ugc external noopener"><?php _e('Learn more', 'classicpress'); ?></a>
 					</p>
 				</div>
 			<?php endif; ?>
@@ -161,7 +161,7 @@
 			?>
 				<div class="donation-box">
 					<p><?php printf(__('Thank developer and support future development of this %s by tipping them.', 'classicpress'), get_post_type()); ?></p>
-					<a href="<?php echo esc_url($donation_link); ?>" target="blank" class="external btn btn-alt" title="<?php _e('Click to tip', 'classicpress'); ?>" rel="nofollow ugc external noopener"><?php printf(__('Tip %s', 'classicpress'), $author_display_name); ?></a>
+					<a href="<?php echo esc_url($donation_link); ?>" target="blank" class="external btn btn-alt" title="<?php esc_attr_e('Click to tip', 'classicpress'); ?>" rel="nofollow ugc external noopener"><?php printf(__('Tip %s', 'classicpress'), $author_display_name); ?></a>
 				</div>
 			<?php endif; ?>
 		</aside>
@@ -179,13 +179,13 @@
 		</div>
 		<div class="software-item-meta">
 			<div class="meta-item left">
-				<a href="<?php echo esc_url(get_post_meta($post->ID, 'download_link', true)); ?>" title="<?php _e('Download', 'classicpress') . ' ' . esc_attr($post->post_title); ?>" target="_blank" rel="noopener noreferrer">
-					<img src="<?php echo esc_url(get_template_directory_uri() . '/images/download.svg'); ?>" alt="<?php _e('Download', 'classicpress'); ?>" width="18" height="18" aria-hidden="true">
+				<a href="<?php echo esc_url(get_post_meta($post->ID, 'download_link', true)); ?>" title="<?php esc_attr_e('Download', 'classicpress') . ' ' . esc_attr($post->post_title); ?>" target="_blank" rel="noopener noreferrer">
+					<img src="<?php echo esc_url(get_template_directory_uri() . '/images/download.svg'); ?>" alt="<?php esc_attr_e('Download', 'classicpress'); ?>" width="18" height="18" aria-hidden="true">
 					<span><?php _e('Download', 'classicpress'); ?></span>
 				</a>
 			</div>
 			<div class="meta-item right">
-				<a href="#info-<?php echo absint($post->ID); ?>" title="<?php _e('More information about', 'classicpress'); ?> <?php echo esc_attr($post->post_title); ?>" class="info-button" data-info="info-<?php echo absint($post->ID); ?>">
+				<a href="#info-<?php echo absint($post->ID); ?>" title="<?php esc_attr_e('More information about', 'classicpress'); ?> <?php echo esc_attr($post->post_title); ?>" class="info-button" data-info="info-<?php echo absint($post->ID); ?>">
 					<img src="<?php echo esc_url(get_template_directory_uri() . '/images/info.svg'); ?>" alt="<?php _e('Info', 'classicpress'); ?>" width="18" height="18" aria-hidden="true">
 					<span><?php _e('More Info', 'classicpress'); ?></span>
 				</a>
