012a1024956416c4ab51421c43068eedb26df507
Viktor Nagornyy <viktor@projectarmy.net>
2023-06-02 11:22:33 -0400

    fix: copyright text adjustments

--- wp-content/themes/classicpress-directory/footer.php
+++ wp-content/themes/classicpress-directory/footer.php
@@ -16,16 +16,16 @@
 		<div class="classic">
 			<div class="footerleft">
 				<a id="footer-logo" href="<?php echo esc_attr('https://www.classicpress.net/'); ?>"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/images/classicpress-logo-feather-white.svg'); ?>" alt="<?php esc_attr_e('ClassicPress feather logo', 'classicpress'); ?>" width="90"></a>
-				<p class="registration"><?php _e('The ClassicPress project is under the direction of The ClassicPress Initiative, a nonprofit organization registered under section 501(c)(3) of the United States IRS code.', 'classicpress'); ?></p>
-
-				<ul  class="social-menu">
-					<li><a href="<?php echo esc_url('https://forums.classicpress.net/'); ?>" target="_blank" title="<?php esc_attr_e('Forums', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-discourse"></i><span class="screen-reader-text"><?php _e('Support forums', 'classicpress'); ?></span></a></li>
-					<li><a href="<?php echo esc_url('https://www.classicpress.net/join-slack/'); ?>" target="_blank" title="<?php esc_attr_e('Slack', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-slack"></i><span class="screen-reader-text"><?php _e('Join on Slack', 'classicpress'); ?></span></a></li>
-					<li><a href="<?php echo esc_url('https://github.com/ClassicPress'); ?>" target="_blank" title="<?php esc_attr_e('GitHub', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-github"></i><span class="screen-reader-text"><?php _e('Visit GitHub', 'classicpress'); ?></span></a></li>
-					<li><a href="<?php echo esc_url('https://fosstodon.org/@classicpress'); ?>" target="_blank" title="<?php esc_attr_e('Mastodon', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-facebook-f"></i><span class="screen-reader-text"><?php _e('Follow on Mastodon', 'classicpress'); ?></span></a></li>
-					<li><a href="<?php echo esc_url('https://twitter.com/GetClassicPress'); ?>" target="_blank" title="<?php esc_attr_e('Twitter', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-twitter"></i><span class="screen-reader-text"><?php _e('Follow on Twitter', 'classicpress'); ?></span></a></li>
-					<li><a href="<?php echo esc_url('https://www.facebook.com/GetClassicPress'); ?>" target="_blank" title="<?php esc_attr_e('Facebook', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-facebook-f"></i><span class="screen-reader-text"><?php _e('Like on Facebook', 'classicpress'); ?></span></a></li>
-				</ul>
+				<div class="registration"><p><?php _e('The ClassicPress project is under the direction of The ClassicPress Initiative, a nonprofit organization registered under section 501(c)(3) of the United States IRS code.', 'classicpress'); ?></p>
+					<ul class="social-menu">
+						<li><a href="<?php echo esc_url('https://forums.classicpress.net/'); ?>" target="_blank" title="<?php esc_attr_e('Forums', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-discourse"></i><span class="screen-reader-text"><?php _e('Support forums', 'classicpress'); ?></span></a></li>
+						<li><a href="<?php echo esc_url('https://www.classicpress.net/join-slack/'); ?>" target="_blank" title="<?php esc_attr_e('Slack', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-slack"></i><span class="screen-reader-text"><?php _e('Join on Slack', 'classicpress'); ?></span></a></li>
+						<li><a href="<?php echo esc_url('https://github.com/ClassicPress'); ?>" target="_blank" title="<?php esc_attr_e('GitHub', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-github"></i><span class="screen-reader-text"><?php _e('Visit GitHub', 'classicpress'); ?></span></a></li>
+						<li><a href="<?php echo esc_url('https://fosstodon.org/@classicpress'); ?>" target="_blank" title="<?php esc_attr_e('Mastodon', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-facebook-f"></i><span class="screen-reader-text"><?php _e('Follow on Mastodon', 'classicpress'); ?></span></a></li>
+						<li><a href="<?php echo esc_url('https://twitter.com/GetClassicPress'); ?>" target="_blank" title="<?php esc_attr_e('Twitter', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-twitter"></i><span class="screen-reader-text"><?php _e('Follow on Twitter', 'classicpress'); ?></span></a></li>
+						<li><a href="<?php echo esc_url('https://www.facebook.com/GetClassicPress'); ?>" target="_blank" title="<?php esc_attr_e('Facebook', 'classicpress'); ?>" rel="noreferrer noopener"><i class="cpicon-facebook-f"></i><span class="screen-reader-text"><?php _e('Like on Facebook', 'classicpress'); ?></span></a></li>
+					</ul>
+				</div>
 			</div>
 			<div class="footerright">
 				<div class="menu-footermenu-container">
@@ -55,7 +55,7 @@
 	<footer id="legal">
 		<div class="cplegal">
 			<div class="cpcopyright">
-				<?php printf(__('© 2018-%s ClassicPress Initiative. All Rights Reserved.', 'classicpress'), date( 'Y' )); ?>
+				<p><?php printf(__('© 2018-%s ClassicPress Initiative. All Rights Reserved.', 'classicpress'), date( 'Y' )); ?></p>
 			</div>
 			<div class="cppolicy">
 				<p><a href="<?php echo esc_url('https://www.classicpress.net/privacy-policy/'); ?>"><?php _e('Privacy Policy', 'classicpress'); ?></a></p>
